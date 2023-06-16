9f46b21602a8f3b1c2b892b5f2d8805b6c4be1c0
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-16 16:52:14 -0700

    low level c prog

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main - lowercase printing
+*
+*Return: return 0
+*/
+
+int main(void)
+{
+
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+
+}
