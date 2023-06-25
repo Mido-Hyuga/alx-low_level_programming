7943804e30e456a5faad6c1c8b18ee70ea3299cd
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-24 18:38:23 -0700

    advanced

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,15 +5,16 @@
 **/
 void print_times_table(int n)
 {
+	int i,j;
 
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
 
-	for (int i = 0; i <= n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (int j = 0; j <= n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int value = i * j;
 
