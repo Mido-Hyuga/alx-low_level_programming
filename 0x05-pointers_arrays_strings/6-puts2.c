5de82da02a7731373b911f54bef9c3477ab7f582
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-27 08:56:02 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,19 @@
 #include "main.h"
+#include <string.h>
+#include <stdio.h>
 /**
- * puts2 - a function.
- *
- *@str: string.
- *
- */
+*puts2 - prints every other character of a string
+*@str: string.
+*/
+
+
 void puts2(char *str)
 {
 	int i;
+	int len = strlen(str);
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
+	for (i = 0; i < len; i++)
 		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-	}
-	_putchar('\n');
+			printf("%c", str[i]);
+	printf("\n");
 }
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,23 +3,21 @@
 #include <string.h>
 
 /**
-*puts_half - print half of a string
-*@str: input to print
-*Return: nothing
-*
+* puts_half - print half of a string
+* @str: input to print
+* Return: nothing
 */
 
 void puts_half(char *str)
 {
 	int i, len;
-	int max_len = strlen(str);
 
-	if (max_len % 2 == 0)
-		len = max_len / 2;
+	len = strlen(str);
+	if (len % 2 == 0)
+		for (i = len / 2; i < len; i++)
+			printf("%c", str[i]);
 	else
-		len = max_len / 2 + 1;
-	for (i = len; i < max_len; i++)
-		printf("%c", str[i]);
+		for (i = len / 2 + 1; i < len; i++)
+			printf("%c", str[i]);
 	printf("\n");
 }
-
