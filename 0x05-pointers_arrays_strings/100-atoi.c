29be2dd356cb4359f6f9f46dc1b5874e69cf8f28
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-27 18:58:31 -0700

    advanced

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _atoi - transform a string to an int
+* @s: the string to be changed
+* Return: the converted int
+*/
+int _atoi(char *s)
+{
+	int i = 1;
+	unsigned int num = 0;
+	while(*s) {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+		s++;
+	}
+	return (num * i);
+}
