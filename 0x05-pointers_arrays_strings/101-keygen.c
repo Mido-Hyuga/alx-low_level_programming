05f61392a59fc370e8235c95d220a3a8fde14f58
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-27 19:50:26 -0700

    advanced pass gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - generates a random password
+*Return: the generated password
+*/
+int main() {
+    char password[12];
+    int i;
+    const char symbols[] = ",.\"'[]{}()!@#$%^&*";
+
+    srand(time(NULL));
+
+    for (i = 0; i < 12; i++) {
+        int type = rand() % 4;
+        if (type == 0) {
+            password[i] = 'A' + rand() % 26; // Uppercase letter
+        } else if (type == 1) {
+            password[i] = 'a' + rand() % 26; // Lowercase letter
+        } else if (type == 2){
+            password[i] = '0' + rand() % 10; // Digit
+        }
+        else {
+             password[i] = symbols[rand() % (sizeof(symbols) - 1)];
+        }
+    }
+
+    password[12] = '\0';
+
+    printf("%s\n", password);
+
+    return 0;
+}
