9343c907f220953f2729d5d19357fd46af19620c
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-06-30 19:22:26 -0700

    1st project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strcat - the concatnate function.
- *
- * @dest: first string.
- * @src: second string.
- * Return: String.
- */
+* _strcat - the concatnate function.
+* @dest: first string.
+* @src: second string.
+* Return: dest.
+*/
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j;
+	int i = 0, j = 0;
 
 	while (i >= 0)
 	{
 		if (*(dest + i) == '\0')
 		{
-			for (j = 0; *(src + j) != '\0'; j++)
+			while (*(src + j) != '\0')
 			{
 				*(dest + i) = *(src + j);
 				i++;
+				j++;
 			}
 			break;
 		}
