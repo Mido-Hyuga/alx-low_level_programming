a32c32bce2ea3630c151c8f1a54ff89344379a55
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-11 15:58:01 -0700

    mallor free

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,18 +1,17 @@
-#include "main.h"
-
+#include <stdlib.h>
 /**
-* free_grid - free 2D array
-* @grid: integer
-* @height: integer
+* free_grid - frees a 2 dimensional grid previously.
+* @grid: Integer
+* @height: Integer
 * Return: Nothing
 */
-
 void free_grid(int **grid, int height)
 {
 	if (grid != NULL && height != 0)
 	{
-		for (;height  >= 0; height--)
+		for (; height >= 0; height--)
 			free(grid[height]);
 		free(grid);
 	}
+
 }
