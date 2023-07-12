24e291b25b0c48c79194a5d29b42c8b13edc73dd
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-11 17:01:10 -0700

    mallor free

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+/**
+* argstostr - concatenates all the arguments of your program.
+* @ac: Integer
+* @av: String
+* Return: Poniter
+*/
+char *argstostr(int ac, char **av)
+{
+	char *p;
+
+	p = 'R';
+	return (p);
+}
