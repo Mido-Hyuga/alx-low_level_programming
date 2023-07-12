f1a93368b3d72058f688dcf4a8236deacffcc860
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-11 19:33:54 -0700

    mallor free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdlib.h>
 /**
 * argstostr - concatenates all the arguments of your program.
@@ -5,10 +6,37 @@
 * @av: String
 * Return: Poniter
 */
+
 char *argstostr(int ac, char **av)
 {
-	char *p;
+	int i, j, k, len = 0, index = 0;
+	char *str;
 
-	p = 'R';
-	return (p);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			len++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (k = 0; av[i][k] != '\0'; k++)
+		{
+			str[index] = av[i][k];
+			index++;
+		}
+		str[index] = '\n';
+		index++;
+	}
+	str[index] = '\0';
+
+	return (str);
 }
