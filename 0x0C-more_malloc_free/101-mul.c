f08d8037bfc27133a2093029a98d7ec9c151ce3f
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-13 14:25:28 -0700

    adv Malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,31 +10,44 @@
 */
 int main(int argc, char **argv)
 {
-	int i = 1, j = 0;
-	unsigned long num;
+	unsigned long num1, num2, result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		exit(98);
+		return (98);
 	}
 
-	while (i <= 2)
+	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
 	{
-		while (argv[i][j] != '\0')
-		{
-			if (isalpha(argv[i][j]))
-			{
-				printf("Error\n");
-				exit(98);
-			}
-			j++;
-		}
+		printf("Error\n");
+		return (98);
+	}
+
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+	result = num1 * num2;
+
+	printf("%lu\n", result);
+
+	return (0);
+}
+
+/**
+* is_valid_number - Check if a string contains only digits.
+* @str: string.
+* Return: 1.
+*/
+int is_valid_number(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
 		i++;
 	}
 
-	num = atol(argv[1]) * atol(argv[2]);
-	printf("%lu\n", num);
-
-	return (0);
+	return (1);
 }
