5a6872081db63ce3bb9447660ddc4a4bea924234
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-13 13:31:25 -0700

    first project

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 /**
-* malloc_checked - Allocate memory using malloc 
+* malloc_checked - Allocate memory using malloc
 * @b: Int
 * Return: return Nothing
 */
 void *malloc_checked(unsigned int b)
 {
 	void *p = malloc(b);
-	
+
 	if (p == NULL)
 		exit(98);
 	return (p);
