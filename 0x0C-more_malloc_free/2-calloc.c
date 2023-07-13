891229206c7b130f2fdf5ddea7ac9af05808a816
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-13 13:40:56 -0700

    easy allocate

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,23 +3,25 @@
 #include "main.h"
 
 /**
- * _calloc - function that concatenates two strings..
- * @nmemb: number of members.
- * @size: size of one memeber..
- *
- * Return: return pointer or 0.
- */
+* _calloc - function that concatenates two strings..
+* @nmemb: number of members.
+* @size: size of one memebers.
+* Return: return pointer or 0.
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
-	unsigned int i;
+	unsigned int i = 0;
 
 	if (nmemb == 0 || size == 0)
 		return (0);
 	p = malloc(nmemb * size);
 	if (p == 0)
 		return (0);
-	for (i = 0; i < nmemb * size; i++)
+	while (i < nmemb * size)
+	{
 		*(p + i) = 0;
+		i++;
+	}
 	return (p);
 }
