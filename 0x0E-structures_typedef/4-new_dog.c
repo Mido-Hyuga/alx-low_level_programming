d3bd94141a64444fa4d23d3fdb6af6c7a1919b00
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-17 16:21:58 -0700

    struct typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,46 +1,49 @@
 #include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* new_dog - create now dog
-* @name: dog name
-* @age: dog age
-* @owner: dog owner
-* Return: dog
+* new_dog - function that creates a new dog.
+* @name: the name of dog.
+* @age: the age of dog.
+* @owner: the owner of dog.
+* Return: retuen 0 or value
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *d;
-	int i = 0, len_name, len_owner;
+	dog_t *d;
+	int i, j, x;
 
-	d = malloc(sizeof(struct dog));
-	if (d == 0 || !(name) || !(owner))
+	d = malloc(sizeof(dog_t));
+	if (d == 0 || owner == 0 || name == 0)
 	{
 		free(d);
 		return (0);
 	}
-	while (*(name + i))
-		i++;
-	len_name = i;
-	i = 0;
-	while (*(owner + i))
-		i++;
-	len_owner = i;
-	(*d).name = malloc(len_name + 1);
-	(*d).owner = malloc(len_owner + 1);
-	if (!((*d).name) || !((*d).owner))
+	for (i = 0; *(name + i) != 0; i++)
+		;
+	for (j = 0; *(owner + j) != 0; j++)
+		;
+
+	d->name = malloc(i + 1);
+	d->owner = malloc(j + 1);
+
+	if (d->name == 0 || d->owner == 0)
 	{
+		free(d->name);
+		free(d->owner);
 		free(d);
-		free((*d).name);
-		free((*d).owner);
 		return (0);
 	}
-	for (i = 0; i < len_name; i++)
-		(*d).name[i] = name[i];
-	(*d).name[i] = 0;
-	(*d).age = age;
-	for (i = 0; i < len_owner; i++)
-		(*d).owner[i] = owner[i];
-	(*d).owner[i] = 0;
+	for (x = 0; x < i; x++)
+		d->name[x] = *(name + x);
+	d->name[x] = '\0';
+
+	d->age = age;
+
+	for (x = 0; x < j; x++)
+		d->owner[x] = *(owner + x);
+	d->owner[x] = '\0';
 	return (d);
 }
