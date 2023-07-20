2fbf5f4401b7910a1e59da9c0df12cd83e5599fc
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-20 16:32:51 -0700

    first project

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -17,7 +17,7 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arg_li, n);
 	while (i < n)
 	{
-		sum += va_arg_li(mehdi, int);
+		sum += va_arg_li(arg_li, int);
 		i++;
 	}
 	va_end(arg_li);
