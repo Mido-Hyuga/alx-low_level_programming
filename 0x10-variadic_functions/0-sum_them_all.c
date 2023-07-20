e07763a71ad0147c2365dbea00fdc56ef95b0933
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-20 16:30:31 -0700

    first project

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,24 +2,24 @@
 
 /**
 * sum_them_all -  function that returns the sum of all its parameters.
-* @n: number of elements.
-* Return: sum of elements
+* @n: number.
+* Return: sum.
 */
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list mehdi;
+	va_list arg_li;
 	unsigned int i = 0;
 	int sum = 0;
 
 	if (n == 0)
 		return (0);
-	va_start(mehdi, n);
+	va_start(arg_li, n);
 	for (i < n)
 	{
-		sum += va_mehdi(mehdi, int);
+		sum += va_arg_li(mehdi, int);
 		i++;
 	}
-	va_end(mehdi);
+	va_end(arg_li);
 	return (sum);
 }
