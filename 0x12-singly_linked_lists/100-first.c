02f857d99f7468dba6cdf8f51199bbb762000c90
Mido-Hyuga <elmehdibouslam@gmail.com>
2023-07-27 17:36:31 -0700

    first adv test

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* before_main - function executed before main
+* Return: no return.
+*/
+
+void before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
